c1a1483ad568ff73a90d4f115a4c3913b03ecb73
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-23 18:24:35 +0300

    Add MailService for sending verification emails using nodemailer

---
+++ services/mail.service.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.MAIL_AUTH_USER,
+                pass: process.env.MAIL_AUTH_PASSWORD
+            }
+        })
+    }
+
+    async sendMail(email, code) {
+        await this.transporter.sendMail({
+            from: process.env.MAIL_AUTH_USER,
+            to: email,
+            subject: "Your verification code",
+            text: "",
+        html: `
+                <div>
+                    <h1>Your verification code is ${code}</h1>
+                </div>
+            `
+        });
+    }
+}
+
+modelu.exports = new MailService();
