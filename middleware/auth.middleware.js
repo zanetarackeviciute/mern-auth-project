1a3a1fad1f705b1a44c3788108908c49c5b5df94
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-24 17:00:05 +0300

    Add authentication middleware to verify user tokens

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+const AuthService = require("../services/auth.service");
+
+const notVerified = {verified: false};
+
+module.exports = async (req, res, next) => {
+    if (req.method === "OPTIONS") {
+        return next();
+    }
+    try {
+        const verified = AuthService.verifyToken(
+            req.headers.authorization.split("")[1]
+        );
+        if (!verified) {
+            return res.json(notVerified)
+        }
+        req.user = verified;
+        next();
+    } catch (e) {
+        console.log(e);
+        res.json(notVerified);
+    }
+}
